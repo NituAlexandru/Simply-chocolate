f717015f1d244e9b67efc01fa4eb67852e4e183e
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2024-02-21 23:35:11 +0200

    Updated top-sellers function
    Updated function so modal with open on click from both 'buy now' buttons

--- src/js/top-sellers.js
+++ src/js/top-sellers.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // Selectează modalul, overlay-ul și butoanele
   const modal = document.querySelector('.buy-now-modal');
-  const modalOverlay = document.querySelector('.modal-overlay'); // Asigură-te că ai acest element în HTML
-  const openModalButton = document.querySelector('[data-modal-open-buy-now]');
+  const modalOverlay = document.querySelector('.modal-overlay');
+  const openModalButtons = document.querySelectorAll(
+    '[data-modal-open-buy-now], [data-modal-open-sec]'
+  );
   const closeModalButton = document.querySelector('[data-modal-close-buy-now]');
 
   function toggleModalVisibility(show) {
@@ -15,8 +16,10 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  openModalButton.addEventListener('click', function () {
-    toggleModalVisibility(true);
+  openModalButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      toggleModalVisibility(true);
+    });
   });
 
   closeModalButton.addEventListener('click', function () {
